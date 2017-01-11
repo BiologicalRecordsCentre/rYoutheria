5061049a2ae7ec272e645ad64dea04b270387062
AugustT <tomaug@ceh.ac.uk>
2017-01-11 11:55:02 +0000

    Change testing so that limited data requests are made on travis. Testing on travis is not minimal for 'getMeasurementData', but will be complete when checked locally.

--- tests/testthat/testgetMeasurementData.R
+++ tests/testthat/testgetMeasurementData.R
@@ -24,41 +24,44 @@ test_that("Testing errors and warnings are given", {
   expect_warning(test <- getMeasurementData(measurementType = 12, MSW05Binomial=c('Tom', 'Dick', 'Harry', 'Petaurus breviceps'), silent=TRUE),
                  regexp = 'There were no results returned for the following species: Tom, Dick, Harry')
   expect_error(test <- getMeasurementData(measurementType = 12, MSW05Binomial='Tom',MSW93Binomial='Dick'),
-               regexp = 'Cannot filter by MSW05Binomial and MSW93Binomial')  
+               regexp = 'Cannot filter by MSW05Binomial and MSW93Binomial')
 })
 
 test_that("Testing search by measurement type", {
   skip_on_cran()
+  skip_on_travis()
   
   expect_is(test <- getMeasurementData(measurementType='Dispersal Age',
                                        silent=TRUE), 'data.frame')
   expect_equal(ncol(test), 32)
-  # expect_is(test2 <- getMeasurementData(c('Growth Data','Dispersal Age'),
-  #                                       silent=TRUE), 'data.frame')
-  # expect_true(nrow(test) < nrow(test2))
-  # expect_is(test3 <- getMeasurementData(measurementType = 'Dispersal Age',
-  #                                      silent = TRUE,
-  #                                      locationData = FALSE), 'data.frame')
-  # expect_equal(ncol(test3), 17)
+  expect_is(test2 <- getMeasurementData(c('Growth Data','Dispersal Age'),
+                                        silent=TRUE), 'data.frame')
+  expect_true(nrow(test) < nrow(test2))
+  expect_is(test3 <- getMeasurementData(measurementType = 'Dispersal Age',
+                                       silent = TRUE,
+                                       locationData = FALSE), 'data.frame')
+  expect_equal(ncol(test3), 17)
   
 })
 
 test_that("Testing MSW93binomial searches", {
   skip_on_cran()
+  skip_on_travis()
   
   expect_is(test <- getMeasurementData(measurementType = 12, MSW93Binomial='Petaurus breviceps', silent=TRUE), 'data.frame')
   expect_equal(ncol(test), 32)
-  # expect_is(test2 <- getMeasurementData(measurementType = 12, MSW93Binomial=c('Petaurus breviceps','Equus zebra'), silent=TRUE), 'data.frame')
-  # expect_true(nrow(test) < nrow(test2))  
+  expect_is(test2 <- getMeasurementData(measurementType = 12, MSW93Binomial=c('Petaurus breviceps','Equus zebra'), silent=TRUE), 'data.frame')
+  expect_true(nrow(test) < nrow(test2))
 })
 
 test_that("Testing MSW05binomial searches", {
   skip_on_cran()
+  skip_on_travis()
   
   expect_is(test <- getMeasurementData(measurementType = 12, MSW05Binomial='Petaurus breviceps', silent=TRUE), 'data.frame')
   expect_equal(ncol(test), 32)
-  # expect_is(test2 <- getMeasurementData(measurementType = 12, MSW05Binomial=c('Petaurus breviceps','Equus zebra'), silent=TRUE), 'data.frame')
-  # expect_true(nrow(test) < nrow(test2))  
+  expect_is(test2 <- getMeasurementData(measurementType = 12, MSW05Binomial=c('Petaurus breviceps','Equus zebra'), silent=TRUE), 'data.frame')
+  expect_true(nrow(test) < nrow(test2))
 })
 
 test_that("Testing complex search", {
@@ -69,25 +72,27 @@ test_that("Testing complex search", {
 
 test_that("Testing casting", {
   skip_on_cran()
+  skip_on_travis()
   
   expect_is(test <- getMeasurementData(measurementType = 12, MSW05Binomial='Petaurus breviceps', cast = FALSE, silent=TRUE), 'data.frame')
   expect_equal(ncol(test), 26)
-  # expect_is(test2 <- getMeasurementData(measurementType = 12, MSW05Binomial='Petaurus breviceps', silent=TRUE), 'data.frame')
-  # expect_true(ncol(test)<ncol(test2))
+  expect_is(test2 <- getMeasurementData(measurementType = 12, MSW05Binomial='Petaurus breviceps', silent=TRUE), 'data.frame')
+  expect_true(ncol(test)<ncol(test2))
 })
 
 test_that("Testing location addition", {
   skip_on_cran()
+  skip_on_travis()
   
   expect_is(test1 <- getMeasurementData(measurementType = 12, locationData = TRUE, silent=TRUE), 'data.frame')
   expect_equal(ncol(test1), 32)
   expect_warning(test2 <- getMeasurementData(measurementType = 12, locationData = TRUE, locationOnly = TRUE, silent=TRUE),
-                 regexp = 'rows have been removed as they are') 
+                 regexp = 'rows have been removed as they are')
   expect_true(nrow(test1) > nrow(test2))
-  # expect_is(test3 <- getMeasurementData(measurementType = 12, country='Switzerland', silent=TRUE), 'data.frame')
-  # expect_true(nrow(test2) > nrow(test3))
-  # expect_is(test4 <- getMeasurementData(measurementType = 12, StudyUnitId=197, silent=TRUE), 'data.frame')  
-  # expect_true(nrow(test2) > nrow(test4))
-  # expect_error(test4 <- getMeasurementData(measurementType = 12, StudyUnitId=197, country = 'India', silent=TRUE),
-  #              regexp = 'Cannot use both StudyUnitId and country at the same time')    
+  expect_is(test3 <- getMeasurementData(measurementType = 12, country='Switzerland', silent=TRUE), 'data.frame')
+  expect_true(nrow(test2) > nrow(test3))
+  expect_is(test4 <- getMeasurementData(measurementType = 12, StudyUnitId=197, silent=TRUE), 'data.frame')
+  expect_true(nrow(test2) > nrow(test4))
+  expect_error(test4 <- getMeasurementData(measurementType = 12, StudyUnitId=197, country = 'India', silent=TRUE),
+               regexp = 'Cannot use both StudyUnitId and country at the same time')
 })
